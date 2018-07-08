ac6dc51d650e15f3f4042ac3f7ace2d2971fac21
Brandon Fergerson <fergerson92@gmail.com>
2018-07-08 15:22:19 -0400

    initial

---
+++ m1/src/main/java/com/company/TestClass.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TestClass {
+    public void otherMethod1() {
+        System.out.println("ok");
+    }
+    public void otherMethod2() {
+        System.out.println("ok");
+    }
+}
---
+++ m2/src/main/java/com/company/TestClass.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TestClass {
+    public void method1() {
+        System.out.println("ok");
+    }
+    public void method2() {
+        System.out.println("ok");
+    }
+}
